1c2b2881c8c9abc23537e28a421c7b21ca5b29fc
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-16 12:31:37 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -10,12 +10,13 @@ public class topManagement{
     string vicepresidentrd; // research & development
 
     static void topDesc(){
-        chiefexecutiveofficer = "Leads the entire company";
-        chiefofstaff = "In charge of hiring for the entire company";
-        vicepresidentproduction = "Responsible for product's manufacturing results";
-        vicepresidentmarketing = "Ensures that the company is known on a public scale";
-        vicepresidentaccounting = "Determines the finance of the entire company to ensure company is financially stable";
-        vicepresidentrd = "Conducts research and effort into evolving the company's current product or service";
+        topManagement mytopManagement = new topManagement;
+        topManagement.chiefexecutiveofficer = "Leads the entire company";
+        topManagement.chiefofstaff = "In charge of hiring for the entire company";
+        topManagement.vicepresidentproduction = "Responsible for product's manufacturing results";
+        topManagement.vicepresidentmarketing = "Ensures that the company is known on a public scale";
+        topMangament.vicepresidentaccounting = "Determines the finance of the entire company to ensure company is financially stable";
+        topManagement.vicepresidentrd = "Conducts research and effort into evolving the company's current product or service";
     }
 }
 
@@ -26,9 +27,10 @@ public class humanresources{
     string benefitsspecialist;
 
     static void hrDesc(){
-        talentacquisitionspecialist = "Responsible for managing and regulating current and new staff";
-        technicalrecruiter = "Conducts hiring digitally via networks such as LinkedIn, AngelList, and company's career site";
-        trainingdevelopmentspecialist = "Provides training and education for company's employees and clients";
+        humanresources myhumanresources = new humanresources;
+        humanresources.talentacquisitionspecialist = "Responsible for managing and regulating current and new staff";
+        humanresources.technicalrecruiter = "Conducts hiring digitally via networks such as LinkedIn, AngelList, and company's career site";
+        humanresources.trainingdevelopmentspecialist = "Provides training and education for company's employees and clients";
     }
 }
 public class engineering{
@@ -45,17 +47,18 @@ public class engineering{
     string directorofengineering;
 
     static void engineeringDesc(){
-        softwareengineerI = "Entry level software engineer hired for development";
-        softwareengineerII = "Mid level software engineer hired for development";
-        seniorsoftwareengineer = "Engineer with at least three or more years in development";
-        principalsoftwarengineer = "Engineer with at least eight years in development and manages other employees";
-        fullstackengineer = "Engineer with experience in back and front end stack technologies to manage the product's application";
-        devopsspecialist = "Utilizes technologies such as Docker and Kubernetes to manage developer teams and their operations";
-        softwareengineerintest = "Manages company's code based on testing and quality assurance";
-        sitereliabilityengineer = "Uses software engineering to automate IT operations and spemd time performing manual IT operations";
-        embeddedsoftwareengineer = "A software engineer that develops on small-scale computer systems";
-        dataanalyst = "An individual that uses company data to make company decisions to improve the overall busines strategy and plans";
-        directorofengineering = "In charge of the chief operations involving all major engineer initiatives and tasks";
+        engineering myengineering = new engineering;
+        engineering.softwareengineerI = "Entry level software engineer hired for development";
+        engineering.softwareengineerII = "Mid level software engineer hired for development";
+        engineering.seniorsoftwareengineer = "Engineer with at least three or more years in development";
+        engineering.principalsoftwarengineer = "Engineer with at least eight years in development and manages other employees";
+        engineering.fullstackengineer = "Engineer with experience in back and front end stack technologies to manage the product's application";
+        engineering.devopsspecialist = "Utilizes technologies such as Docker and Kubernetes to manage developer teams and their operations";
+        engineering.softwareengineerintest = "Manages company's code based on testing and quality assurance";
+        engineering.sitereliabilityengineer = "Uses software engineering to automate IT operations and spemd time performing manual IT operations";
+        engineering.embeddedsoftwareengineer = "A software engineer that develops on small-scale computer systems";
+        engineering.dataanalyst = "An individual that uses company data to make company decisions to improve the overall busines strategy and plans";
+        engineering.directorofengineering = "In charge of the chief operations involving all major engineer initiatives and tasks";
     }
 }
 public class customersuccess{
@@ -65,10 +68,11 @@ public class customersuccess{
     string customersupportspecialist;
 
     static void CSdesc(){
-        technicalsupportspecialist = "Assists clients with their problems or questions about the company's product";
-        itanalyst = "Supports help tickets made from the clients and employees and manages such issues";
-        customersuccessmanager = "In charge of leading and collaborating with other customer success specialists regarding questions about the company's product";
-        customersupportspecialist = "Helps customers with their problems about the product";
+        customersuccess mycustomersuccess = new customersuccess;
+        customersuccess.technicalsupportspecialist = "Assists clients with their problems or questions about the company's product";
+        customersuccess.itanalyst = "Supports help tickets made from the clients and employees and manages such issues";
+        customersuccess.customersuccessmanager = "In charge of leading and collaborating with other customer success specialists regarding questions about the company's product";
+        customersuccess.customersupportspecialist = "Helps customers with their problems about the product";
     }
 }
 public class finance{
@@ -80,12 +84,13 @@ public class finance{
     string collectionsanalyst;
 
     static void financeDesc(){
-        junioraccountant = "Responsible to manage the company's finanials in an entry level role";
-        senioraccountant = "Reponsible to manage the company's financial statements with seniority";
-        compensationspecialist = "Determines the internal employees with their pay";
-        auditor = "Checks to ensure that company spending is responsible and to measure profit/loss";
-        budgetanalyst = "Responsible for allocating the spending of each department";
-        collectionsanalyst = "Assesses the riskiness of a company in credit extension and repayment collection";
+        finance myfinance = new finance;
+        finance.junioraccountant = "Responsible to manage the company's finanials in an entry level role";
+        finance.senioraccountant = "Reponsible to manage the company's financial statements with seniority";
+        finance.compensationspecialist = "Determines the internal employees with their pay";
+        finance.auditor = "Checks to ensure that company spending is responsible and to measure profit/loss";
+        finance.budgetanalyst = "Responsible for allocating the spending of each department";
+        finance.collectionsanalyst = "Assesses the riskiness of a company in credit extension and repayment collection";
     }
 }
 public class marketing{
@@ -96,11 +101,12 @@ public class marketing{
     string brandmanager;
 
     static void marketingDesc(){
-        digitalmarketingspecialist = "Ensures that the company's vision and news about the company is marketed digitally";
-        seospecialist = "Manages search engine queries that show the company more frequently";
-        socialmediamanager = "Gain impressions and views across social media networks including LinkedIn, Facebook, Instagram, and Twitter";
-        visualdesigner = "Creates and manages company's web, digital, and print assets";
-        brandmanager = "Oversees market research for the company and demonstrates how the company's product or service is presented to the company";
+        marketing mymarketing = new marketing;
+        marketing.digitalmarketingspecialist = "Ensures that the company's vision and news about the company is marketed digitally";
+        marketing.seospecialist = "Manages search engine queries that show the company more frequently";
+        marketing.socialmediamanager = "Gain impressions and views across social media networks including LinkedIn, Facebook, Instagram, and Twitter";
+        marketing.visualdesigner = "Creates and manages company's web, digital, and print assets";
+        marketing.brandmanager = "Oversees market research for the company and demonstrates how the company's product or service is presented to the company";
     }
 }
 public class product{
@@ -112,11 +118,12 @@ public class product{
     string productmanager;
 
     static void productDesc(){
-        productionmanager = "Identifies the customer need and the larger business objectives that a product or feature will fulfill";
-        productionsupervisor = "Oversees the operations of the company to ensure production of a product or service";
-        graphicdesigner = "Creates and manages various logos, designs, and images for the company";
-        productdesigner = "Manages the proces of imagining, creating, and iterating products that solve users' problems";
-        uxdesigner = "Envisions the best user experience of a product or service to help them navigate and create new flows";
+        product myproduct = new product;
+        product.productionmanager = "Identifies the customer need and the larger business objectives that a product or feature will fulfill";
+        product.productionsupervisor = "Oversees the operations of the company to ensure production of a product or service";
+        product.graphicdesigner = "Creates and manages various logos, designs, and images for the company";
+        product.productdesigner = "Manages the proces of imagining, creating, and iterating products that solve users' problems";
+        product.uxdesigner = "Envisions the best user experience of a product or service to help them navigate and create new flows";
     }
 }
 public class sales{
@@ -128,12 +135,13 @@ public class sales{
     string regionalsalesmanager;
 
     static void salesDesc(){
-        businessdevelopmentrepresentative = "A sales professional dedicated to finding new business and tapping into new markets";
-        corporateaccountexecutive = "Define and execute sales plans for the assigned territory forecast, meet and exceed quota ";
-        accountexecutive = "Primary contact between a vendor and customer";
-        insidesalespresentative = "Responsible for a variety of sales tasks, including cold calls and emails";
-        outsidesalesrepresentative = "Responsible of leading sales in the field and territory, potentially selling the product or service directly";
-        regionalsalesmanager = "Responsible for developing and implementing sales, marketing, financial and structural strategies in stores across a group of territories";
+        sales mysales = new sales;
+        sales.businessdevelopmentrepresentative = "A sales professional dedicated to finding new business and tapping into new markets";
+        sales.corporateaccountexecutive = "Define and execute sales plans for the assigned territory forecast, meet and exceed quota ";
+        sales.accountexecutive = "Primary contact between a vendor and customer";
+        sales.insidesalespresentative = "Responsible for a variety of sales tasks, including cold calls and emails";
+        sales.outsidesalesrepresentative = "Responsible of leading sales in the field and territory, potentially selling the product or service directly";
+        sales.regionalsalesmanager = "Responsible for developing and implementing sales, marketing, financial and structural strategies in stores across a group of territories";
     }
 }
 public class security{
@@ -142,9 +150,10 @@ public class security{
     string staffsecurityengineer;
 
     static void securityDesc(){
-        securityengineer = "Handles the digital security for their company or clients";
-        seniorcomplianceengineer = "Ensures the company's technical compliance with applicable laws and regulations";
-        staffsecurityengineer = "Respond to security incidents, search threats across areas of the company, and find systematic bugs";
+        security mysecurity = new security;
+        security.securityengineer = "Handles the digital security for their company or clients";
+        security.seniorcomplianceengineer = "Ensures the company's technical compliance with applicable laws and regulations";
+        security.staffsecurityengineer = "Respond to security incidents, search threats across areas of the company, and find systematic bugs";
     }
 }
 
