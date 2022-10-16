8e0d45c6e554c72839cb310f0d2e6eb9d2e17d8b
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-16 09:52:59 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -7,8 +7,16 @@ public class topManagement{
     string vicepresidentmarketing;
     string vicepresidentaccounting;
     string vicepresidentengineering;
-    string headmanufacturing;
     string vicepresidentrd; // research & development
+
+    static void topDesc(){
+        chiefexecutiveofficer = "Leads the entire company";
+        chiefofstaff = "In charge of hiring for the entire company";
+        vicepresidentproduction = "Responsible for product's manufacturing results";
+        vicepresidentmarketing = "Ensures that the company is known on a public scale";
+        vicepresidentaccounting = "Determines the finance of the entire company to ensure company is financially stable";
+        vicepresidentrd = "Conducts research and effort into evolving the company's current product or service";
+    }
 }
 
 public class humanresources{
@@ -16,6 +24,12 @@ public class humanresources{
     string technicalrecruiter;
     string trainingdevelopmentspecialist;
     string benefitsspecialist;
+
+    static void hrDesc(){
+        talentacquisitionspecialist = "Responsible for managing and regulating current and new staff";
+        technicalrecruiter = "Conducts hiring digitally via networks such as LinkedIn, AngelList, and company's career site";
+        trainingdevelopmentspecialist = "Provides training and education for company's employees and clients";
+    }
 }
 public class engineering{
     string softwareengineerI;
@@ -23,26 +37,56 @@ public class engineering{
     string seniorsoftwareengineer;
     string principalsoftwarengineer;
     string fullstackengineer;
+    string devopsspecialist;
     string softwareengineerintest;
     string sitereliabilityengineer;
     string embeddedsoftwareengineer;
     string dataanalyst;
     string directorofengineering;
+
+    static void engineeringDesc(){
+        softwareengineerI = "Entry level software engineer hired for development";
+        softwareengineerII = "Mid level software engineer hired for development";
+        seniorsoftwareengineer = "Engineer with at least three or more years in development";
+        principalsoftwarengineer = "Engineer with at least eight years in development and manages other employees";
+        fullstackengineer = "Engineer with experience in back and front end stack technologies to manage the product's application";
+        devopsspecialist = "Utilizes technologies such as Docker and Kubernetes to manage developer teams and their operations";
+        softwareengineerintest = "Manages company's code based on testing and quality assurance";
+        sitereliabilityengineer = "Uses software engineering to automate IT operations and spemd time performing manual IT operations";
+        embeddedsoftwareengineer = "A software engineer that develops on small-scale computer systems";
+        dataanalyst = "An individual that uses company data to make company decisions to improve the overall busines strategy and plans";
+        directorofengineering = "In charge of the chief operations involving all major engineer initiatives and tasks";
+    }
 }
 public class customersuccess{
     string technicalsupportspecialist;
     string itanalyst;
     string customersuccessmanager;
     string customersupportspecialist;
+
+    static void CSdesc(){
+        technicalsupportspecialist = "Assists clients with their problems or questions about the company's product";
+        itanalyst = "Supports help tickets made from the clients and employees and manages such issues";
+        customersuccessmanager = "In charge of leading and collaborating with other customer success specialists regarding questions about the company's product";
+        customersupportspecialist = "Helps customers with their problems about the product";
+    }
 }
 public class finance{
     string junioraccountant;
     string senioraccountant;
     string compensationspecialist;
     string auditor;
-    string taxconsulting;
     string budgetanalyst;
     string collectionsanalyst;
+
+    static void financeDesc(){
+        junioraccountant = "Responsible to manage the company's finanials in an entry level role";
+        senioraccountant = "Reponsible to manage the company's financial statements with seniority";
+        compensationspecialist = "Determines the internal employees with their pay";
+        auditor = "Checks to ensure that company spending is responsible and to measure profit/loss";
+        budgetanalyst = "Responsible for allocating the spending of each department";
+        collectionsanalyst = "Assesses the riskiness of a company in credit extension and repayment collection";
+    }
 }
 public class marketing{
     string digitalmarketingspecialist;
@@ -50,6 +94,14 @@ public class marketing{
     string socialmediamanager;
     string visualdesigner;
     string brandmanager;
+
+    static void marketingDesc(){
+        digitalmarketingspecialist = "Ensures that the company's vision and news about the company is marketed digitally";
+        seospecialist = "Manages search engine queries that show the company more frequently";
+        socialmediamanager = "Gain impressions and views across social media networks including LinkedIn, Facebook, Instagram, and Twitter";
+        visualdesigner = "Creates and manages company's web, digital, and print assets";
+        brandmanager = "Oversees market research for the company and demonstrates how the company's product or service is presented to the company";
+    }
 }
 public class product{
     string productionmanager;
@@ -58,20 +110,41 @@ public class product{
     string productdesigner;
     string uxdesigner;
     string productmanager;
+
+    static void productDesc(){
+        productionmanager = "Identifies the customer need and the larger business objectives that a product or feature will fulfill";
+        productionsupervisor = "Oversees the operations of the company to ensure production of a product or service";
+        graphicdesigner = "Creates and manages various logos, designs, and images for the company";
+        productdesigner = "Manages the proces of imagining, creating, and iterating products that solve users' problems";
+        uxdesigner = "Envisions the best user experience of a product or service to help them navigate and create new flows";
+    }
 }
 public class sales{
     string businessdevelopmentrepresentative;
     string corporateaccountexecutive;
     string accountexecutive;
-    string federalsalesmanager;
     string insidesalespresentative;
     string outsidesalesrepresentative;
     string regionalsalesmanager;
-    string areasalesdirector;
+
+    static void salesDesc(){
+        businessdevelopmentrepresentative = "A sales professional dedicated to finding new business and tapping into new markets";
+        corporateaccountexecutive = "Define and execute sales plans for the assigned territory forecast, meet and exceed quota ";
+        accountexecutive = "Primary contact between a vendor and customer";
+        insidesalespresentative = "Responsible for a variety of sales tasks, including cold calls and emails";
+        outsidesalesrepresentative = "Responsible of leading sales in the field and territory, potentially selling the product or service directly";
+        regionalsalesmanager = "Responsible for developing and implementing sales, marketing, financial and structural strategies in stores across a group of territories";
+    }
 }
 public class security{
     string securityengineer;
     string seniorcomplianceengineer;
     string staffsecurityengineer;
+
+    static void securityDesc(){
+        securityengineer = "Handles the digital security for their company or clients";
+        seniorcomplianceengineer = "Ensures the company's technical compliance with applicable laws and regulations";
+        staffsecurityengineer = "Respond to security incidents, search threats across areas of the company, and find systematic bugs";
+    }
 }
 
