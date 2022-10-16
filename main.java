7c0ab4d36746c64f7e5b8a1246fe87d16af78630
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-16 08:56:24 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,77 @@
+// Marvish Chandra
+
+public class topManagement{
+    string chiefexecutiveofficer;
+    string chiefofstaff;
+    string vicepresidentproduction;
+    string vicepresidentmarketing;
+    string vicepresidentaccounting;
+    string vicepresidentengineering;
+    string headmanufacturing;
+    string vicepresidentrd; // research & development
+}
+
+public class humanresources{
+    string talentacquisitionspecialist;
+    string technicalrecruiter;
+    string trainingdevelopmentspecialist;
+    string benefitsspecialist;
+}
+public class engineering{
+    string softwareengineerI;
+    string softwareengineerII;
+    string seniorsoftwareengineer;
+    string principalsoftwarengineer;
+    string fullstackengineer;
+    string softwareengineerintest;
+    string sitereliabilityengineer;
+    string embeddedsoftwareengineer;
+    string dataanalyst;
+    string directorofengineering;
+}
+public class customersuccess{
+    string technicalsupportspecialist;
+    string itanalyst;
+    string customersuccessmanager;
+    string customersupportspecialist;
+}
+public class finance{
+    string junioraccountant;
+    string senioraccountant;
+    string compensationspecialist;
+    string auditor;
+    string taxconsulting;
+    string budgetanalyst;
+    string collectionsanalyst;
+}
+public class marketing{
+    string digitalmarketingspecialist;
+    string seospecialist;
+    string socialmediamanager;
+    string visualdesigner;
+    string brandmanager;
+}
+public class product{
+    string productionmanager;
+    string productionsupervisor;
+    string graphicdesigner;
+    string productdesigner;
+    string uxdesigner;
+    string productmanager;
+}
+public class sales{
+    string businessdevelopmentrepresentative;
+    string corporateaccountexecutive;
+    string accountexecutive;
+    string federalsalesmanager;
+    string insidesalespresentative;
+    string outsidesalesrepresentative;
+    string regionalsalesmanager;
+    string areasalesdirector;
+}
+public class security{
+    string securityengineer;
+    string seniorcomplianceengineer;
+    string staffsecurityengineer;
+}
+
